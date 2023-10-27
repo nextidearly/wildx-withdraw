5635513258f89549d41d328ac6981ffc67c72a22
nextidearly <talenteddev0125@gamil.com>
2023-10-26 18:34:25 -0700

    balance checker

--- next.config.js
+++ next.config.js
@@ -1,9 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: config => {
+  webpack: (config) => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
     return config;
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/apirone/:slug*",
+        destination: "https://apirone.com/api/v2/:slug*",
+      },
+      {
+        source: "/mempool/:slug*",
+        destination: "https://mempool.space/api/:slug*",
+      },
+    ];
   },
 };
 
